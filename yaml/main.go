9e8f31912f8d46956dc0c0c295c39b2e69c41081
Bhanu Kiran Chaluvadi <bkch@universal-robots.com>
2021-12-09 11:14:01 +0100

    Added basic YAML Parsing

---
+++ yaml/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"gopkg.in/yaml.v3"
+)
+
+type MetaData struct {
+	ApiVersion string `yaml:"apiVersion"`
+	Kind       string `yaml:"kind"`
+	URCap      urCap
+	Artifacts  artifacts
+}
+
+type urCap struct {
+	Name    string `yaml:"name"`
+	Vendor  string `yaml:"vendor"`
+	Version string `yaml:"version"`
+}
+
+type artifacts struct {
+	Containers  []container  `yaml:"containers", omitempty`
+	Osgibundles []osgibundle `yaml:"osgibundles", omitempty`
+	Webarchives []webarchive `yaml:"webarchives", omitempty`
+}
+type container struct {
+	Name         string        `yaml:"name"`
+	Image        string        `yaml:"image"`
+	Ports        []port        `yaml:"ports,flow"`
+	VolumeMounts []volumeMount `yaml:"volumeMounts",flow`
+}
+
+type port struct {
+	Name          string `yaml:"name"`
+	ContainerPort int    `yaml:"containerPort"`
+	Protocol      string `yaml:"protocol"`
+}
+
+type volumeMount struct {
+	MountPath string `yaml:"mountPath"`
+}
+
+type osgibundle struct {
+	Name   string `yaml:"name"`
+	Bundle string `yaml:"bundle"`
+}
+
+type webarchive struct {
+	Name   string `yaml:"name"`
+	Bundle string `yaml:"bundle"`
+}
+
+func readMetaData(path string) (*MetaData, error) {
+	buff, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	t := &MetaData{}
+	err = yaml.Unmarshal(buff, t)
+	if err != nil {
+		return nil, fmt.Errorf("in file %q: %v", path, err)
+	}
+	fmt.Printf("--- t:\n%v\n\n", *t)
+
+	d, err := yaml.Marshal(&t)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	fmt.Printf("--- t dump:\n%s\n\n", string(d))
+
+	m := make(map[interface{}]interface{})
+	err = yaml.Unmarshal([]byte(buff), &m)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	fmt.Printf("--- m:\n%v\n\n", m)
+
+	d, err = yaml.Marshal(&m)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	fmt.Printf("--- m dump:\n%s\n\n", string(d))
+
+	return t, nil
+}
+
+func main() {
+	data, err := readMetaData("urcap.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%v", *data)
+}
