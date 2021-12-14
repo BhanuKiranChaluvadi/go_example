d465fc8f9aa983fac1b385a3b653e0fc5709bb68
Bhanu Kiran Chaluvadi <nthdimensionalreality@gmail.com>
2021-12-14 07:46:29 +0100

    Added Json Parser

---
+++ README.md
@@ -0,0 +1,7 @@
+
+
+go get gopkg.in/yaml.v2
+
+go get github.com/pkg/errors
+
+go run schema.go
---
+++ urservice/schema/schema.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/pkg/errors"
+	"gopkg.in/yaml.v2"
+)
+
+func Load(baseFilePath string) error {
+	bytes, err := ioutil.ReadFile(baseFilePath)
+	if err != nil {
+		return err
+	}
+
+	baseFile, err := parseConfig(bytes)
+	if err != nil {
+		return err
+	}
+
+	b, err := json.Marshal(baseFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(b))
+	return nil
+}
+
+func parseConfig(b []byte) (map[string]interface{}, error) {
+	yml, err := ParseYAML(b)
+	if err != nil {
+		return nil, err
+	}
+	return yml, err
+}
+
+// ParseYAML reads the bytes from a file, parses the bytes into a mapping
+// structure, and returns it.
+func ParseYAML(source []byte) (map[string]interface{}, error) {
+	var cfg interface{}
+	if err := yaml.Unmarshal(source, &cfg); err != nil {
+		return nil, err
+	}
+	cfgMap, ok := cfg.(map[interface{}]interface{})
+	if !ok {
+		return nil, errors.Errorf("Top-level object must be a mapping")
+	}
+	converted, err := convertToStringKeysRecursive(cfgMap, "")
+	if err != nil {
+		return nil, err
+	}
+	return converted.(map[string]interface{}), nil
+
+}
+
+// keys need to be converted to strings for jsonschema
+func convertToStringKeysRecursive(value interface{}, keyPrefix string) (interface{}, error) {
+	if mapping, ok := value.(map[interface{}]interface{}); ok {
+		dict := make(map[string]interface{})
+		for key, entry := range mapping {
+			str, ok := key.(string)
+			if !ok {
+				return nil, formatInvalidKeyError(keyPrefix, key)
+			}
+			var newKeyPrefix string
+			if keyPrefix == "" {
+				newKeyPrefix = str
+			} else {
+				newKeyPrefix = fmt.Sprintf("%s.%s", keyPrefix, str)
+			}
+			convertedEntry, err := convertToStringKeysRecursive(entry, newKeyPrefix)
+			if err != nil {
+				return nil, err
+			}
+			dict[str] = convertedEntry
+		}
+		return dict, nil
+	}
+	if list, ok := value.([]interface{}); ok {
+		var convertedList []interface{}
+		for index, entry := range list {
+			newKeyPrefix := fmt.Sprintf("%s[%d]", keyPrefix, index)
+			convertedEntry, err := convertToStringKeysRecursive(entry, newKeyPrefix)
+			if err != nil {
+				return nil, err
+			}
+			convertedList = append(convertedList, convertedEntry)
+		}
+		return convertedList, nil
+	}
+	return value, nil
+}
+
+func formatInvalidKeyError(keyPrefix string, key interface{}) error {
+	var location string
+	if keyPrefix == "" {
+		location = "at top level"
+	} else {
+		location = fmt.Sprintf("in %s", keyPrefix)
+	}
+	return errors.Errorf("Non-string key %s: %#v", location, key)
+}
+
+func main() {
+	err := Load("example.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
